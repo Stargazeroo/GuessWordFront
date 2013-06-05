050df44703e3e48a04b961d87886a7dc8aefad06
anikotc <andruxanikolenko@gmail.com>
2013-06-05 17:44:18 +0300

    Add model login

---
+++ javascripts/1P/templates/model_log_in.js
@@ -0,0 +1,34 @@
+var log_in = Backbone.View.extend({
+    fields: {    
+		login: {
+			type: "text",
+			name: "login",
+			id: "login"
+		}
+		password: {
+			type: "password",
+			name: "pass",
+			id: "pass"
+		}
+	}				
+	button: {
+		log_in_button: {
+			submit: "submit",
+			name: "submit_button",
+			id: "submit_button"
+		}
+	}
+
+
+
+//initialize: function(){
+
+}
+
+
+
+
+
+
+
+});
