d8361facfda5f17d30b0aa1a71b189bdfb8dbc41
sergejgasjuk <gasjuksergej@mail.ru>
2013-06-11 19:40:27 +0300

    indentation fixed

--- javascripts/mainSectionAnimation.js
+++ javascripts/mainSectionAnimation.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $(function() {
-       $("div#mainSection a ").hover(function() {
+        $("div#mainSection a ").hover(function() {
             var el = this;
             $(this).addClass('rotate').css("background-image", "linear-gradient(to bottom, #1C3368, #494B54)");
             setTimeout(function() {
