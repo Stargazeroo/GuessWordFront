76ba84aaa104ffd0b4408db06f0e31e0ae5d6bb9
Viktor <abramov90@gmail.com>
2013-06-12 18:43:36 +0300

    Update mainMenuView.js
    modified with controller

--- javascripts/1P/templates/mainMenuView.js
+++ javascripts/1P/templates/mainMenuView.js
@@ -1,22 +1,19 @@
 var ListView = Backbone.View.extend({
     el: $('body'),
-    formRegistr: $('#regFormDiv'),
-
+    
     // `initialize()`: Automatically called upon instantiation. Where you make 
     //all types of bindings, _excluding_ UI events, such as clicks, etc.
-    initialize: function(){
+    initialize: function(regModelInputFields, regModelButtons){
+        
         _.bindAll(this, 'render'); // fixes loss of context for 'this' within methods
-        this.render(); // not all views are self-rendering. This one is.
+        this.render(regModelInputFields, regModelButtons); // not all views are self-rendering. This one is.
     },
     // `render()`: Function in charge of rendering the entire view in `myDiv`. Needs to be manually called by the user.
-    render: function(){
-        var data = new registrationForm();
-        var inputFields = data.get('fields');
-        var button = new registrationForm();
-        var buttonGet = button.get('buttons');
-        var regForm = new EJS({url:'/javascripts/1P/templates/regPageTemplate.ejs'}).render(inputFields);
+    render: function(regModelInputFields, regModelButtons){
+        
+        var regForm = new EJS({url:'/javascripts/1P/templates/regPageTemplate.ejs'}).render(regModelInputFields);
         this.$el.append(regForm);
-        var confirmButton = new EJS ({url:'/javascripts/1P/templates/regPageTemplate.ejs'}) .render(buttonGet);
+        var confirmButton = new EJS ({url:'/javascripts/1P/templates/regPageTemplate.ejs'}) .render(regModelButtons);
         this.$el.append(confirmButton);
     }
 });
