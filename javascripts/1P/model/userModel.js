992ddba4a10dc2f087ded98d2b4a8b50fd0a4ec9
irushtc <iryna.rushchyshyn@gmail.com>
2013-07-04 02:04:27 +0300

    Added a model which contains all static info for user templates, and also takes data from localstorage

--- javascripts/1P/model/userModel.js
+++ javascripts/1P/model/userModel.js
@@ -1,8 +1,26 @@
 var userModel = Backbone.Model.extend({
     defaults: {
+        logout: "app_logout",
         trainingResults: "app_trainingResults",
         matchResults   : "app_matchResults",
         userAge: "app_age",
-        userLocation: "app_location"
+        userLocation: "app_location",
+        trainingAttributes: {
+            trainings: "app_trainings",
+            wordsCorrect: "app_wordsCorrect",
+            wordsIncorrect: "app_wordsIncorrect",
+            trainingTime: "app_trainingTime",
+            totalScore: "app_totalScore",
+            ratio: "app_ratio"
+        },
+    },
+
+    initialize: function(){
+        var main = JSON.parse(localStorage['main']);
+        this.set("main", main);
+        var training = JSON.parse(localStorage['training']);
+        if (training) {
+            this.set("training", training);
+        }
     }
 });
